db572e2cfef0e625bd38d7932bb2d6b0773b7175
Korathejo <87575791+Korathejo@users.noreply.github.com>
2022-05-30 01:40:12 +0100

    Add files via upload

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Sonic_Mania")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Escola-PC")> 
+<Assembly: AssemblyProduct("Sonic_Mania")> 
+<Assembly: AssemblyCopyright("Copyright © Escola-PC 2017")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("76b4626e-cd45-4b2b-9c37-128c064a676c")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
